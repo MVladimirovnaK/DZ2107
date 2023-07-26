98f4b41351a8ebb00558008c1f521a9a721eca8c
mariya <mariyakudryashova19980308@gmail.com>
2023-07-26 22:36:07 +0300

    add 01

---
+++ 01/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Random rnd = new Random();
+int count = rnd.Next(1, 10);
+Console.WriteLine($"Количество: {count}");
+int size = count;
+int[] numbers = new int[size];
+for(int i = 0; i < size; i++)
+{
+    Console.Write($"Введите {i+1} число: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
+Console.WriteLine();
+
+int counter = 0;
+for ( int i = 0; i < size; i++)
+{
+   if (numbers[i]>0)counter++;
+}
+Console.WriteLine($"Количество чисел больше 0 - {counter}.");
